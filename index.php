7456bd92a177a19c1cf0a59adc7da11843b8de3a
Bagman <zagr.gadel@gmail.com>
2024-12-31 02:37:05 +0300

    Показывать ошибки

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 define("BOT_API_KEY","8013304224:AAEIb0K8rrTKxL0NIZyQGN4gZO6B_MA8CSM");
 
 require_once 'ApiClient.php';
