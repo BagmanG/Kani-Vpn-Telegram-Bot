2b41347912365c05d4324923fead7601d2cb2d60
Bagman <zagr.gadel@gmail.com>
2024-12-31 02:40:35 +0300

    test

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ error_reporting(E_ALL);
 define("BOT_API_KEY","8013304224:AAEIb0K8rrTKxL0NIZyQGN4gZO6B_MA8CSM");
 
 require_once 'Classes/Telegram.php';
+require_once 'Commands/StartCommand.php';
 
 $telegram = new Telegram(BOT_API_KEY);
 
