a3c9b523f0193ec648498cdbe0837c34e5c48ad9
Bagman <zagr.gadel@gmail.com>
2025-01-02 01:13:37 +0300

    fix

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $telegram = new Telegram($_ENV['TG_BOT_API']);
 
 $telegram->addCommand(new StartCommand($telegram, "/start"));
 $telegram->addCommand(new HelpCommand($telegram, "/help"));
+$telegram->addCommand(new TestCommand($telegram, "/test"));
 
 $telegram->run();
 ?>
