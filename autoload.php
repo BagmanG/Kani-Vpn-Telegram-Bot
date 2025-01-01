096ee6c7b0e7e2f748fd2adfa541338e8a3cbdb6
Bagman <zagr.gadel@gmail.com>
2025-01-02 00:27:01 +0300

    autoload

--- autoload.php
+++ autoload.php
@@ -1,5 +1,17 @@
 <?
-spl_autoload_register(function ($class_name) {
-    include $class_name . '.php';
+$directories = [
+    __DIR__ . '/Classes', // Классы
+    __DIR__ . '/Commands', // Комманды
+];
+
+spl_autoload_register(function ($class_name) use ($directories) {
+    $file_name = str_replace('\\', DIRECTORY_SEPARATOR, $class_name) . '.php';
+    foreach ($directories as $directory) {
+        $file = $directory . DIRECTORY_SEPARATOR . $file_name;
+        if (file_exists($file)) {
+            include_once $file;
+            return;
+        }
+    }
 });
 ?>
