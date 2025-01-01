5484469e1c41ae3c3828704264b3060b63e803af
Bagman <zagr.gadel@gmail.com>
2025-01-02 00:52:59 +0300

    Update Core.php

--- Classes/Core.php
+++ Classes/Core.php
@@ -3,7 +3,7 @@
 class Core
 {
     private $env;
-    private const ENV_FILE_PATH = './'.__DIR__ . '/.env';
+    private const ENV_FILE_PATH = __DIR__ . '/../.env';
 
     public function __construct()
     {
