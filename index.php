60f2824916c0e3205b28b8f0cee63c5fa28a1ae5
Bagman <zagr.gadel@gmail.com>
2025-01-02 00:46:54 +0300

    fix

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ define("BOT_API_KEY","8013304224:AAEIb0K8rrTKxL0NIZyQGN4gZO6B_MA8CSM");
 require_once 'autoload.php';
 
 $core = new Core();
-$core->init();
+$core->Init();
 
 $telegram = new Telegram(BOT_API_KEY);
 
