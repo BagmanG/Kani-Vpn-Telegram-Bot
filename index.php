1001af7d8fecc6bd3181224dc96971ba78a46d8d
Bagman <zagr.gadel@gmail.com>
2025-01-02 00:54:34 +0300

    fix

--- index.php
+++ index.php
@@ -3,14 +3,12 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-define("BOT_API_KEY","8013304224:AAEIb0K8rrTKxL0NIZyQGN4gZO6B_MA8CSM");
-
 require_once 'autoload.php';
 
 $core = new Core();
 $core->Init();
 
-$telegram = new Telegram(BOT_API_KEY);
+$telegram = new Telegram($_ENV['TG_BOT_API']);
 
 $telegram->addCommand(new StartCommand($telegram, "/start"));
 $telegram->addCommand(new HelpCommand($telegram, "/help"));
