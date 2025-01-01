b54a88b8a35727da661ae5e5e6455a227234ed0a
Bagman <zagr.gadel@gmail.com>
2025-01-02 00:56:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,6 @@ require_once 'autoload.php';
 $core = new Core();
 $core->Init();
 
-echo $_ENV['TG_BOT_API'];
-exit();
 $telegram = new Telegram($_ENV['TG_BOT_API']);
 
 $telegram->addCommand(new StartCommand($telegram, "/start"));
