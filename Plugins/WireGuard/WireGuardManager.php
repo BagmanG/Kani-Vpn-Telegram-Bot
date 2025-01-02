6211887c26e8b8ad6a22231759b6f011551bf11c
Bagman <zagr.gadel@gmail.com>
2025-01-02 03:45:12 +0300

    fix

--- Plugins/WireGuard/WireGuardManager.php
+++ Plugins/WireGuard/WireGuardManager.php
@@ -1,8 +1,8 @@
 <?
 class WireGuardManager{
     public static function CreateNewConfig($serverId){
-        $server = Database::fetch("SELECT ip FROM servers WHERE id = $serverId");
-        Telegram::sendMessageFromCallback("IP:".$server['ip']);
+        $server = Database::fetch("SELECT ip,port,api_key FROM servers WHERE id = $serverId");
+        Telegram::sendMessageFromCallback("IP:".$server['ip']."_".$server['port']."_".$server['api_key']);
     }
 }
 ?>
