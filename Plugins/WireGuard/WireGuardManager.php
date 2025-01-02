0dfa91fa80ad2e139057c97b8e3ad7b6714a1881
Bagman <zagr.gadel@gmail.com>
2025-01-02 04:35:13 +0300

    test

--- Plugins/WireGuard/WireGuardManager.php
+++ Plugins/WireGuard/WireGuardManager.php
@@ -34,8 +34,8 @@ class WireGuardManager
         $configId = $configData['configId'];
         $serverId = $configData['serverId'];
         $serverData = Database::fetch("SELECT id,ip,port,api_key FROM servers WHERE id = $serverId");
-        $imageUrl = $_ENV['SERVER_ROOT']."Plugins/WireGuard/WireGuardFile.php?server_port=".$serverData['ip'].":".$serverData['port']."&token=".$serverData['api_key']."&id=".$configId;
-        Telegram::sendPhotoWithCaption($imageUrl,"Вы успешно создали конфигурацию.");
+        $fileUrl = $_ENV['SERVER_ROOT']."Plugins/WireGuard/WireGuardFile.php?server_port=".$serverData['ip'].":".$serverData['port']."&token=".$serverData['api_key']."&id=".$configId;
+        Telegram::sendDocument($fileUrl,"Файл конфигурации.");
     }
 }
 ?>
