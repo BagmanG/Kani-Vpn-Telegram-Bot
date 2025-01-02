94d38a1a0445452402568907ab23219d2dac9881
Bagman <zagr.gadel@gmail.com>
2025-01-02 04:19:29 +0300

    f

--- Plugins/WireGuard/WireGuardQr.php
+++ Plugins/WireGuard/WireGuardQr.php
@@ -1,8 +1,11 @@
 <?
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 require 'WireGuardAPI.php';
 $token = $_GET['token'];
 $server = $_GET['server'];
-$id = 3;
+$id = $_GET['id'];
 $apiClient = new WireGuardAPI('http://'.$server, $token);
 header('Content-Type: image/png');
 $clientConf = $apiClient->getClientById($id, 'qr');
