8cacc6cf07074f364dd9569e0bf8b81fe136b3a5
Bagman <zagr.gadel@gmail.com>
2025-01-02 20:22:59 +0300

    awdaw

--- Classes/Telegram.php
+++ Classes/Telegram.php
@@ -155,7 +155,7 @@ class Telegram
     }
 
     public static function getUserState(){
-        return Database::fetch("SELECT state FROM users WHERE id = ".self::getUserId())['state'];
+        return Database::fetch("SELECT state FROM users WHERE userId = ".self::getUserId())['state'];
     }
 
     public static function tryParseCallback($callbackData)
